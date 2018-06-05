116bfcae4754c85081c4f7e4f417be3716baa1ed
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-06-05 11:37:31 +0200

    Fix exports.upload in handler.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -11,24 +11,36 @@ exports.welcome = function(request,response) {
   });
 }
 
-exports.upload = function(request,response) {
-  console.log(("Rozpoczynam obsługę żadania upload.").green);
-  var form = new formidable.IncomingForm(); //????
-  form.parse(request, function(error, fields, files){ //????
-    // fs.renameSync(files.upload.path, "test.png");     //???
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("received image:<br/>");
-    response.write("<img src='/show' />");
-    response.end();
+//  my bad solution
+// exports.upload = function(request,response) {
+//   console.log(("Rozpoczynam obsługę żadania upload.").green);
+//   var form = new formidable.IncomingForm(); //????
+//   form.parse(request, function(error, fields, files){ //????
+//     // fs.renameSync(files.upload.path, "test.png");     //???
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("received image:<br/>");
+//     response.write("<img src='/show' />");
+//     response.end();
+//   });
+//   fs.readFile('templates/upload.html', function(err, html){
+//     response.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+//     response.write(html);
+//     response.end();
+//   });
+// }
+
+exports.upload = function(request, response) {
+  console.log(("Rozpoczynam obsługę żądania upload.").green);
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(error, fields, files) {
+      fs.renameSync(files.upload.path, "test.png");  //
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("received image:<br/>");
+      response.write("<img src='/show' />");
+      response.end();
   });
-  fs.readFile('templates/upload.html', function(err, html){
-    response.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
-    response.write(html);
-    response.end();
-  });
-  // response.write("Rozpoczynam upload!");
-  // response.end();
 }
+
 
 exports.show = function(request, response) {
   console.log(("Rozpoczyna obsługę żądania show").green);
@@ -42,6 +54,11 @@ exports.show = function(request, response) {
 
 exports.error = function(request, response) {
   console.log(('Nie wiem co robić').red);
-  response.write('404');
-  response.end();
+  fs.readFile('templates/error.html', function(err,html) {
+    response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+    response.write(html);
+    response.end();
+  });
+  // response.write('404');
+  // response.end();
 }
